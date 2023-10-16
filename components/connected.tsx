95a2ea49450e0d5250c0be26d03e66b0fbd02cfc
ciaranightingale <ciara.nightingale@sky.com>
2023-10-16 21:37:55 +0100

    improve user journey

--- components/connected.tsx
+++ components/connected.tsx
@@ -14,27 +14,43 @@ import { activeChain } from "../const";
 import toast from "react-hot-toast";
 import toastStyle from "../utils/toastConfig";
 import styles from "../styles/Home.module.css";
+import { useEffect } from "react";
+import { Dispatch } from "react";
+import { SetStateAction } from "react";
 
 interface ConnectedProps {
-  signer: Signer | undefined;
+  signer?: Signer | undefined;
+  setText?: Dispatch<SetStateAction<string>>;
 }
 
-export const SessionKeyConnected: React.FC<ConnectedProps> = ({ signer }) => {
+export const SessionKeyConnected: React.FC<ConnectedProps> = ({
+  signer,
+  setText,
+}) => {
   return (
     <ThirdwebSDKProvider
       signer={signer}
       activeChain={activeChain}
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
     >
-      <Connected />
+      <Connected setText={setText} />
     </ThirdwebSDKProvider>
   );
 };
 
-const Connected = () => {
+const Connected: React.FC<ConnectedProps> = ({ setText }) => {
   const address = useAddress();
   const { contract: editionDropContract } = useContract(editionDrop);
   const { data: nfts } = useOwnedNFTs(editionDropContract, address);
+
+  useEffect(() => {
+    if (setText) {
+      setText(
+        "Interacting with the blockchain is now gasless and transactionless!"
+      );
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
       <Web3Button
--- pages/index.tsx
+++ pages/index.tsx
@@ -32,45 +32,43 @@ const Home: NextPage = () => {
   const [signer, setSigner] = useState<Signer>();
   return (
     <div className={styles.container}>
-      <div className={styles.main}>
-        <div className={styles.border}>
-          <h1 className={styles.title}>
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {project}
-              </a>{" "}
-            </span>
-          </h1>
-          <p className={styles.description}>{text}</p>
-          {address ? (
-            hasSessionKey ? (
-              <div className={styles.container}>
-                {signer ? (
-                  <>
-                    <SessionKeyConnected signer={signer} />
-                  </>
-                ) : (
-                  <h2>loading...</h2>
-                )}
-              </div>
-            ) : (
-              <Agree
-                setHasSessionKey={setHasSessionKey}
-                setSigner={setSigner}
-                setText={setText}
-                project={project}
-              />
-            )
-          ) : (
+      <div className={styles.border}>
+        <h1 className={styles.title}>
+          <span className={styles.gradientText0}>
+            <a
+              href="https://thirdweb.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {project}
+            </a>{" "}
+          </span>
+        </h1>
+        <p className={styles.description}>{text}</p>
+        {address ? (
+          hasSessionKey ? (
             <div className={styles.container}>
-              <ConnectWallet />
+              {signer ? (
+                <>
+                  <SessionKeyConnected signer={signer} setText={setText} />
+                </>
+              ) : (
+                <h2>loading...</h2>
+              )}
             </div>
-          )}
-        </div>
+          ) : (
+            <Agree
+              setHasSessionKey={setHasSessionKey}
+              setSigner={setSigner}
+              setText={setText}
+              project={project}
+            />
+          )
+        ) : (
+          <div className={styles.container}>
+            <ConnectWallet />
+          </div>
+        )}
       </div>
     </div>
   );
